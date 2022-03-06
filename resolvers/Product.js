e020d0c302aa2caa02aa68b3651a7d795e28b1bd
eugenehan94 <eugene.han94@gmail.com>
2022-03-05 20:46:29 -0500

    graphql tutorial

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -3,4 +3,7 @@
     category: ({categoryId}, args, {categories}) => {
       return categories.find((category) => category.id === categoryId);
     },
+    reviews: ({id}, args, {reviews}) => {
+      return reviews.filter(review => review.productId === id)
+    }
   }
