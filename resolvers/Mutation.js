57c79cee4189046350bbebe8e8c85573eb6eca5d
eugenehan94 <eugene.han94@gmail.com>
2022-03-12 11:57:44 -0500

    graphql delete mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -62,5 +62,14 @@ exports.Mutation = {
             else return product;   
         })
         return true
+    },
+    deleteProduct: (parent, {id}, {db}) => {
+        db.products = db.products.filter(product => product.id !== id);
+        db.reviews = db.reviews.filter(review => review.productId !== id)
+        return true;
+    },
+    deleteReview: (parent, {id}, {db}) => {
+        db.reviews = db.reviews.filter(review => review.id !== id)
+        return true;
     }
 }
