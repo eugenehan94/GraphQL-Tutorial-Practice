10c168833bdce5072789241fc40bc5d93dee53de
eugenehan94 <eugene.han94@gmail.com>
2022-03-13 13:48:07 -0400

    graphql update

--- schema.js
+++ schema.js
@@ -14,6 +14,9 @@ exports.typeDefs = gql`
     deleteCategory(id: ID!): Boolean!
     deleteProduct(id: ID!): Boolean!
     deleteReview(id: ID!): Boolean!
+    updateCategory(id: ID!, input: UpdateCategoryInput!): Category
+    updateProduct(id: ID!, input: UpdateProductInput!): Product
+    updateReview(id: ID!, input: UpdateReviewInput!): Review
   }
   type Product {
     id: ID!
@@ -45,6 +48,9 @@ exports.typeDefs = gql`
   input AddCategoryInput {
     name: String!
   }
+  input UpdateCategoryInput {
+    name: String!
+  }
   input AddProductInput {
     name: String!
     description: String!
@@ -52,7 +58,16 @@ exports.typeDefs = gql`
     image: String!
     price: Float!
     onSale: Boolean!
-    categoryId: String!
+    categoryId: String
+  }
+  input UpdateProductInput {
+    name: String!
+    description: String!
+    quantity: Int!
+    image: String!
+    price: Float!
+    onSale: Boolean!
+    categoryId: String
   }
   input AddReviewInput {
     date: String!
@@ -61,4 +76,11 @@ exports.typeDefs = gql`
     rating: Int!
     productId: ID!
   }
+  input UpdateReviewInput {
+    date: String!
+    title: String!
+    comment: String!
+    rating: Int!
+    productId: ID!
+  } 
 `;
